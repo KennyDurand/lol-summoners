dd1aa410fdf88b4d7ce93900512849ebd5b7fdc2
Kenny Durand <durand.kenny@gmail.com>
2013-11-24 21:51:06 +0100

    Added Facebook share

--- app/js/app.js
+++ app/js/app.js
@@ -15,4 +15,67 @@ config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/home'});
 }]);
 
-var jQuery = angular.element;
+$(function() {
+	// Additional JS functions here
+	  window.fbAsyncInit = function() {
+	    FB.init({
+	      appId      : 643848432313317, // App ID
+	      status     : true,    // check login status
+	      cookie     : true,    // enable cookies to allow the
+	                            // server to access the session
+	      xfbml      : true     // parse page for xfbml or html5
+	                            // social plugins like login button below
+	    });
+
+	    // Put additional init code here
+	  };
+
+	// Load the SDK Asynchronously
+	  (function(d, s, id){
+	     var js, fjs = d.getElementsByTagName(s)[0];
+	     if (d.getElementById(id)) {return;}
+	     js = d.createElement(s); js.id = id;
+	     js.src = "//connect.facebook.net/en_US/all.js";
+	     fjs.parentNode.insertBefore(js, fjs);
+	   }(document, 'script', 'facebook-jssdk'));
+
+
+
+	$('#fb-post').click(function() {
+		FB.login(function(response) {
+		    if (response.authResponse && response.status == 'connected') {
+		        FB.api(
+		       'https://graph.facebook.com/me/objects/lol-summoners:game',
+		       'post',
+		       { object: {
+					"title":"Ezreal (4/6/12)",				
+					"og:url":'http://www.google.com',
+					"image":'http://www.team-ldlc.com/wp-content/uploads/2012/10/Ezreal_Square_0.png',
+					"summoner_name":"Terenoth",
+					"kills":"4",
+					"deaths":"6",
+					"assists":"12",
+					"champion_name":"Ezreal",
+					"description":""
+		       	},
+		         privacy: {'value': 'SELF'},
+		         access_token: response.authResponse.accessToken },
+		       function(response) {
+		         if (!response) {
+		           alert('Error occurred.');
+		         } else if (response.error) {
+		           document.getElementById('fb-post').innerHTML =
+		             'Error: ' + response.error.message;
+		         } else {
+		           document.getElementById('fb-post').innerHTML =
+		             '<a href=\"https://www.facebook.com/me/activity/' +
+		             response.id + '\">' +
+		             'Story created.  ID is ' +
+		             response.id + '</a>';
+		         }
+		       }
+		    );
+		    }
+		});
+	});
+});
