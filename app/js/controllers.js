d19c42aec2cb0047c0c5befd78ff2b64addedca4
Kenny Durand <durand.kenny@gmail.com>
2013-12-05 17:33:20 +0100

    First steps of profile page

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,15 +6,17 @@ angular.module('LoLSummoners.controllers', []).
   controller('HomeCtrl', [function() {
 
   }]).
-  controller('SearchSummonerCtrl', ['$scope', '$http', function SearchSummonerCtrl($scope, $http) {
+  controller('SearchSummonerCtrl', ['$scope', '$http', '$location', '$rootScope', function SearchSummonerCtrl($scope, $http, $location, $rootScope) {
 	$scope.summonerName = '';
 	$scope.serverName = 'EUW';
+	$scope.data = null;
 
 	$scope.search = function(summonerName, serverName) {
 		var url = 'http://137.194.11.186:8124/?summonerName=' + summonerName + '&serverName=' + serverName;
 
 		$http({method: 'GET', url: url}).success(function(data) {
-	    	alert('YOUPI');
+			$rootScope.playerData = data.data;
+			$location.path('/profile');
 	  	}).
 	  	error(function(data, status, headers, config) {
 	    	$scope.searchForm.$setValidity('Request to the stats API failed.', false);
@@ -22,6 +24,23 @@ angular.module('LoLSummoners.controllers', []).
 		
 	}
   }])
-  .controller('ProfileCtrl', [function() {
+  .controller('ProfileCtrl', ['$scope', '$rootScope', function ProfileCtrl($scope, $rootScope) {
+	$scope.playerData = $rootScope.playerData;
 
+	$scope.getSum = function(dataType) {
+		var count = 0;
+		$scope.playerData.ranked_stats.lifetimeStatistics.forEach(function(stat) {
+			if (stat.statType == dataType) {
+				count += stat.value;
+			}
+		});
+
+		return count;
+	}
+
+	$scope.countGames = $scope.getSum('TOTAL_SESSIONS_PLAYED');
+
+	$scope.getAverage = function(dataType) {
+		return (parseFloat($scope.getSum(dataType)) / $scope.countGames).toFixed(1);
+	}
   }]);
